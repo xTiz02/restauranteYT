639ca7f8e1eac60414c20397f37fc03ce1e39944
PieroGit <u21231696@utp.edu.pe>
2023-12-21 13:37:51 -0500

    Primer commit

---
+++ src/main/java/org/prd/restaurante/RestauranteApplication.java
@@ -0,0 +1,14 @@
+package org.prd.restaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteApplication {
+
+    public static void main(String[] args) {
+        System.out.println();
+        SpringApplication.run(RestauranteApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/prd/restaurante/prueba/Prueba.java
@@ -0,0 +1,7 @@
+package org.prd.restaurante.prueba;
+
+public class Prueba {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
